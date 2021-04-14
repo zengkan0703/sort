037ad7bcaaca039518559f7d24122b373df0edc4
zeng.kan <zeng.kan@immomo.com>
2021-04-14 12:06:05 +0800

    feat: 冒泡排序优化

--- bubble.js
+++ bubble.js
@@ -1,12 +1,20 @@
-function bubbleSort(arr, type = 'asc') {
-  let n = arr.length;
-  while(n) {
-    for (let i = 0, length = arr.length; i < length; i ++) {
-      if (type === 'asc' ? arr[i + 1] < arr[i] : arr[i + 1] > arr[i]) {
-        [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]]; //交换位置
+// 冒泡排序
+function bubbleSort(arr) {  
+  // 优化标记
+  const exchange = false;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1- i; j++) {
+      if (arr[j] > arr[j+1]) {
+        const temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        exchange = true;
+      }
+      // 如果一次循环中没有交换操作，说明当前的数组已经有序了，直接返回即可
+      if (!exchange) {
+        return arr;
       }
     }
-    n --
   }
   return arr;
 }
